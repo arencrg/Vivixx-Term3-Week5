67299542ae475b64bed72affa2d28ea1e7b1aacc
arencrg <clarence13cariaga@gmail.com>
2017-09-28 12:15:15 +0800

    Github Dashboard API

--- reactwithJamie/github_dashboard/src/App.js
+++ reactwithJamie/github_dashboard/src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state = {
+    data: []
+  }
+}
+
+  componentDidMount(){
+    console.log("Trying to fetch")
+    fetch("https://api.github.com/users/octocat")
+    .then(function(response){
+        return response.json();
+      })
+    .then((response) => {
+        this.setState({data: response});
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>GITHUB DASHBOARD API</h1>
+        <h3>{this.state.data.name}</h3>
       </div>
     );
   }
